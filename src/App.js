38d73f690e611d803d9563aa25727f9f69789143
Liviu Schera <liviu115@gmail.com>
2018-05-20 10:53:59 +0930

    first commit for nobind

--- src/App.js
+++ src/App.js
@@ -3,10 +3,39 @@ import "./App.css";
 import "tachyons";
 
 class App extends Component {
-   render() {
-      return fetch("https://swapi.co/api/planets/1")
+   constructor() {
+      super();
+      this.state = {
+         planet: ""
+      };
+      // this.getVal = this.getVal.bind(this);
+   }
+
+   //
+   //    getVal() {
+   //       fetch("https://swapi.co/api/planets/1")
+   //          .then(response => response.json())
+   //          .then(data => {
+   //             console.log(data.name);
+   //             this.setState({
+   //                planet: data.name
+   //             });
+   //          });
+   //    }
+
+   componentDidMount() {
+      fetch("https://swapi.co/api/planets/1")
          .then(response => response.json())
-         .then(data => console.log(data));
+         .then(data => {
+            console.log(data.name);
+            this.setState({
+               planet: data.name
+            });
+         });
+   }
+
+   render() {
+      return <div>Here we are: {this.state.planet}</div>;
    }
 }
 
